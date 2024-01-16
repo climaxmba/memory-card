81f6972a19b6f9754ba0bc818fc55914c3a05c92
Climax Mba <climaxium150@gmail.com>
2024-01-16 16:50:26 +0100

    Set up side effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,60 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Pokedex } from "pokeapi-js-wrapper";
 import Cards from "./components/Cards";
 import { Instructions, Scores } from "./components/utils";
 import "./styles/App.css";
 
+const pokeapi = new Pokedex();
+
 function App() {
   const [pokemons, setPokemons] = useState([]),
-    [score, setScore] = useState([]);
+    [score, setScore] = useState([]),
+    [clickedIds, setClickedIds] = useState([]),
+    [currOffset, setCurrOffset] = useState(1);
 
   const states = {
     pokemons,
     setPokemons,
     score,
-    setScore
+    setScore,
+    clickedIds,
+    setClickedIds,
+    currOffset,
+    setCurrOffset,
   };
 
   function resetScore() {
     // TODO
     return;
   }
+
+  useEffect(() => {
+    const limit = 10;
+
+    if (clickedIds.length === limit)
+      (async () => {
+        try {
+          const pokemonData = await pokeapi.getPokemonsList({
+            limit,
+            offset: currOffset,
+          });
+          setPokemons(
+            pokemonData.map((data) => {
+              return { name: data.name, id: data.id, sprite: data.sprite };
+            })
+          );
+        } catch (error) {
+          // TODO: Display Error
+          console.error(error);
+        }
+      })();
+    else {
+      // Shuffle pokemons
+      console.log("else");
+      setPokemons([...pokemons].sort(() => Math.random() - 0.5));
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [clickedIds.length]);
 
   return (
     <>
